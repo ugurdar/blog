4170aaaec6102b235307b03e2c2ddd0d6e08c445
ugurdar <ugurdarr@gmail.com>
2025-07-28 02:50:33 +0300

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/blog/' : '/',  
+  base: process.env.NODE_ENV === 'production'
+    ? '/blog/'     
+    : '/',
 });
